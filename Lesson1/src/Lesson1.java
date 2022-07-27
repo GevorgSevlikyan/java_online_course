023c72c56b37ecdfec084a5dfb6d19651b3a8561
Gevorg Sevlikyan <you@example.comgevsevlikyan@gmail.com>
2022-07-27 15:13:49 +0400

    My first git commit

---
+++ Lesson1/src/Lesson1.java
@@ -0,0 +1,32 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Barev");
+        System.out.println("Lesson 1");
+        byte x = 127;
+        System.out.println(x);
+        short y = -4568;
+        System.out.println(y);
+        int a = 454644;
+        System.out.println(a);
+        long b = 4786782862L;
+        System.out.println(b);
+        float aa = 455.4f;
+        double bb = 45.4;
+        boolean v = true;
+        System.out.println(v);
+        v = false;
+        System.out.println(v);
+        char p = '$';
+        System.out.println(p);
+        char tt = '\u003F';
+        System.out.println(tt);
+        String l = "Barev Gevorg";
+        System.out.println(l);
+        String firstName = "Gevorg";
+        String lastName = "Sevlikyan";
+        System.out.println(firstName +" "+ lastName);
+        int k = 95;
+        System.out.println("k = "+ k);
+
+    }
+}
